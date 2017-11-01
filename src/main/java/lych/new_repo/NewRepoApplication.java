c2fd76d91127635f7ae0866cdb59b4f166360da6
Dmitry Lych <dmitrylych@gmail.com>
2017-11-01 12:47:16 +0200

    first commit

---
+++ src/main/java/lych/new_repo/NewRepoApplication.java
@@ -0,0 +1,12 @@
+package lych.new_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewRepoApplication.class, args);
+	}
+}
---
+++ src/test/java/lych/new_repo/NewRepoApplicationTests.java
@@ -0,0 +1,16 @@
+package lych.new_repo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewRepoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
